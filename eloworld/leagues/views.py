80a3d2b8740d4496b23d3c8c93a328a1655a093a
Mark Uvari <muvari@gmail.com>
2017-07-15 11:39:45 -0400

    Implement simple elo rating

--- eloworld/leagues/views.py
+++ eloworld/leagues/views.py
@@ -9,7 +9,7 @@ def view_league(request, league_name):
     league_ = League.objects.get(name=league_name)
 
     player_list = []
-    players = Player.objects.order_by('rating')
+    players = league_.players.order_by('-rating')
     for p in players:
         total_points = 0
         opponent_points = 0
@@ -23,8 +23,11 @@ def view_league(request, league_name):
                 opponent_points += m.matchparticipant_set.all()[0].score
 
         diff = (total_points - opponent_points) / p.matches.count()
+        diffStr = str(round(diff, 2))
+        if diff > 0:
+            diffStr = '+' + diffStr
 
-        p_list = {'name': p.name, 'matches': p.matches.count(), 'diff': diff, 'rating': p.rating}
+        p_list = {'name': p.name, 'matches': p.matches.count(), 'diff': diffStr, 'rating': p.rating}
         player_list.append(p_list)
 
 
@@ -36,9 +39,45 @@ def new_league(request):
     
 def add_match(request, league_name):
     league_ = League.objects.get(name=league_name)
+    redScore = request.POST.get("redscore", 0)
+    blueScore = request.POST.get("bluescore", 0)
     redPlayer, rcreated = Player.objects.get_or_create(name=request.POST.get("redname", ""), league=league_)
     bluePlayer, bcreated = Player.objects.get_or_create(name=request.POST.get("bluename", ""), league=league_)
     newMatch = Match.objects.create(time=datetime.datetime.now(), league=league_)
-    rmp = MatchParticipant.objects.create(player=redPlayer, match=newMatch, score=request.POST.get("redscore", 0), wasRed=True)
-    bmp = MatchParticipant.objects.create(player=bluePlayer, match=newMatch, score=request.POST.get("bluescore", 0), wasRed=False)
+    rmp = MatchParticipant.objects.create(player=redPlayer, match=newMatch, score=redScore, wasRed=True)
+    bmp = MatchParticipant.objects.create(player=bluePlayer, match=newMatch, score=blueScore, wasRed=False)
+
+    redRating = redPlayer.rating
+    blueRating = bluePlayer.rating
+
+    redExp = expected(redRating, blueRating)
+    blueExp = expected(blueRating, redRating)
+
+    newRedElo = elo(redRating, redExp, redScore < blueScore)
+    newBlueElo = elo(blueRating, blueExp, blueScore < redScore)
+
+    redPlayer.rating = newRedElo
+    bluePlayer.rating = newBlueElo
+    redPlayer.save()
+    bluePlayer.save()
+    
     return redirect(f'/l/{league_name}/')
+
+def expected(A, B):
+    """
+    Calculate expected score of A in a match against B
+    :param A: Elo rating for player A
+    :param B: Elo rating for player B
+    """
+    return 1 / (1 + 10 ** ((B - A) / 400))
+
+
+def elo(old, exp, score, k=32):
+    """
+    Calculate the new Elo rating for a player
+    :param old: The previous Elo rating
+    :param exp: The expected score for this match
+    :param score: The actual score for this match
+    :param k: The k-factor for Elo (default: 32)
+    """
+    return old + k * (score - exp)
